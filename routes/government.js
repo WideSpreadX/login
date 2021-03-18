df8ea66106911ad62cbfe3162e6aad75bf70d64f
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2021-03-18 12:16:39 -0400

    took out api connection in government

--- routes/government.js
+++ routes/government.js
@@ -8,7 +8,7 @@ const {ensureAuthenticated } = require('../config/auth');
 
 
 router.get('/', ensureAuthenticated, (req, res) => {
-    const state = 'NJ';
+/*     const state = 'NJ';
 	const options = {
         method: 'GET',
         url: `http://www.opensecrets.org/api/?method=getLegislators&id=${state}&apikey=${process.env.OPEN_SECRETS_API_KEY}&output=json`
@@ -17,10 +17,10 @@ router.get('/', ensureAuthenticated, (req, res) => {
         axios.request(options).then(function (response) {
             const returnedData = response.data;
             console.log(returnedData)
-            res.render('gov-home', {currentPageTitle: 'Government Home'})
         }).catch(function (error) {
             console.error(error);
-        });
+        }); */
+        res.render('gov-home', {currentPageTitle: 'Government Home'})
 });
 
     
