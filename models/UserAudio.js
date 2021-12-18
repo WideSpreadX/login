44934e16c2f79a90c570648a72918d7306433b18
Jonathan Olson <48499429+WideSpreadX@users.noreply.github.com>
2021-12-17 22:15:01 -0500

    added UserAudio.js

---
+++ models/UserAudio.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userAudioSchema = new mongoose.Schema({
+    audio_owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    album: String,
+    song: String,
+    artist: String,
+    genre: String,
+    tags: [String],
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+module.exports = new mongoose.model('UserAudio', userAudioSchema);
